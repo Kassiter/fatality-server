1fcfa8f1f9b4878a3072d10dba9f7d100c9a5202
o.volosianskyi <o.volosianskyi@sloboda-studio.com>
2020-08-05 21:52:05 +0300

    Migrations fix

--- db/migrate/20200802174305_change_report_in_contest_keys.rb
+++ db/migrate/20200802174305_change_report_in_contest_keys.rb
@@ -1,5 +1,6 @@
 class ChangeReportInContestKeys < ActiveRecord::Migration[6.0]
   def change
+    remove_column :contest_keys, :report, :string
     add_column :contest_keys, :report, :text
   end
 end
